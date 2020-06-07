8055106259458ce24783e2295335c6af4feb8d0b
Evan Jones <evan.a.jones3@gmail.com>
2020-06-07 18:51:36 -0400

    Fix top-level __init__.py

--- activelearner/__init__.py
+++ activelearner/__init__.py
@@ -1,5 +1,24 @@
-"""Top-level package for ActiveLearner."""
+"""
+Top-level package for ActiveLearner.
+
+Available submodules
+--------------------
+
+dataset
+    Defines dataset extensions
+interfaces
+    Defines interfaces and dataset objects
+labeler
+    Interface for labeling data
+models
+    Machine learning models for predicting labels
+strategies
+    Active learning query strategies
+"""
 
 __author__ = """Evan Andrew Jones"""
 __email__ = 'evan.a.jones3@gmail.com'
 __version__ = '0.1.0'
+
+__all__ = ['dataset', 'interfaces', 'labeler', 'models', 'strategies']
+
