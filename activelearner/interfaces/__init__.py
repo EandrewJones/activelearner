bad81ad4903d48a815825eefd2d4ac4f8d002f76
Evan Jones <evan.a.jones3@gmail.com>
2020-06-07 20:39:16 -0400

    add all class imports to interface __init__

--- activelearner/interfaces/__init__.py
+++ activelearner/interfaces/__init__.py
@@ -2,7 +2,19 @@
 Interface Concrete Classes
 """
 from .interfaces import Dataset 
+from .interfaces import Labeler
+from .interfaces import Query
+from .interfaces import Model
+from .interfaces import ProbabilisticModel
+from .interfaces import ContinuousModel
+from .interfaces import MultilabelModel
 
 __all__ = [
-    'Dataset'
+    'Dataset',
+    'Labeler',
+    'Query',
+    'Model',
+    'ProbabilisticModel',
+    'ContinuousModel',
+    'MultilabelModel'
 ]
