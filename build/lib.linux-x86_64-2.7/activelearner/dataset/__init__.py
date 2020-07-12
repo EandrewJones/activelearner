27d3e13d276d55d69e517a13d8bda5e2b34190c1
Evan Jones <evan.a.jones3@gmail.com>
2020-07-12 13:29:22 -0400

    untrack directories

--- build/lib.linux-x86_64-2.7/activelearner/dataset/__init__.py
+++
@@ -1,8 +0,0 @@
-"""
-Concrete dataset classes
-"""
-from .textdataset import TextDataset
-
-__all__ = [
-    'TextDataset'
-]
--- build/lib.linux-x86_64-2.7/activelearner/labeler/__init__.py
+++
@@ -1,8 +0,0 @@
-"""
-Concrete labeler classes
-"""
-from .askoracle import AskOracle
-
-__all__ = [
-    'AskOracle'
-]
