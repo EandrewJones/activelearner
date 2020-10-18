ddff16ffcae95f9641c94e62739d57dee28981bd
Evan Jones <evan.a.jones3@gmail.com>
2020-10-18 19:51:04 -0400

    Bump version: 0.3.2 → 0.3.3

--- setup.py
+++ setup.py
@@ -66,6 +66,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/EandrewJones/activelearner',
-    version='0.3.2',
+    version='0.3.3',
     zip_safe=False,
 )
