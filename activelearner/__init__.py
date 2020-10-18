6491ba790cb166f652ebe56ad8a41ec27564e6b6
Evan Jones <evan.a.jones3@gmail.com>
2020-10-18 19:04:24 -0400

    Bump version: 0.2.2 → 0.3.0

--- activelearner/__init__.py
+++ activelearner/__init__.py
@@ -18,7 +18,7 @@ strategies
 
 __author__ = """Evan Andrew Jones"""
 __email__ = 'evan.a.jones3@gmail.com'
-__version__ = '0.2.2'
+__version__ = '0.3.0'
 
 import activelearner.dataset
 import activelearner.interfaces
